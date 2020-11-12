9abb2488388a8bc8d89144eabc1467a50c05ee06
jiang <392711804@qq.com>
2020-11-12 21:07:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# redos
+
+## References
+- [writing-an-os-in-rust](https://github.com/rustcc/writing-an-os-in-rust)
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn on_panic(info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
