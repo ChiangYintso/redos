a16f406c6ab98dc636f3dbc109d6a1ef28cb7007
jiang <392711804@qq.com>
2020-11-27 14:35:54 +0800

    refactor to support x86_64 and riscv64imac

--- src/vga/src/lib.rs
+++ src/vga/src/lib.rs
@@ -1,12 +1,15 @@
 #[cfg(target_arch = "x86_64")]
-pub fn put_acsii_char(ch: u8) {
+pub fn puts(s: &[u8]) {
     static VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
     unsafe {
-
+        for (i, &byte) in s.iter().enumerate() {
+            unsafe {
+                *VGA_BUFFER.offset(i as isize * 2) = byte;
+                *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb;
+            }
+        }
     }
 }
 
 #[cfg(target_arch = "riscv64imac")]
-pub fn put_ascii_char(ch: u8) {
-
-}
+pub fn puts(s: &[u8]) {}
